b06014ca8d07b685acbfd7437d1b42b8613f722d
Samuel <samuelbsantosn@gmail.com>
2021-08-22 20:39:42 -0300

    adding segim

--- src/tst.py
+++ src/tst.py
@@ -18,50 +18,48 @@ images_json = database["images"]
 data = np.array(images_json[0]["image"]).astype("uint8")
 
 im = cv.imread('/Users/samuelbrasileiro/Desktop/euu.png')
+print(data)
+p = Bia.Picture(im)
+
+x = p.imageSegmentation()
 # data = np.array([np.concatenate(column) for column in data])
-gray = cv.cvtColor(data, cv2.COLOR_RGB2GRAY)
-gray = cv.GaussianBlur(gray, (3,3), 0, 0, cv.BORDER_DEFAULT)
+# gray = cv.cvtColor(p.__data, cv2.COLOR_RGB2GRAY)
+# gray = cv.GaussianBlur(gray, (3,3), 0, 0, cv.BORDER_DEFAULT)
 
-dft = cv.dft(np.float32(gray),flags = cv.DFT_COMPLEX_OUTPUT)
+# dft = cv.dft(np.float32(gray),flags = cv.DFT_COMPLEX_OUTPUT)
 
-dft_shift = np.fft.fftshift(dft)
-#dft_shift = cv2.GaussianBlur(dft_shift, (3, 1), 7)
-
-
-magnitude_spectrum = 20*np.log(cv.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
+# dft_shift = np.fft.fftshift(dft)
+# #dft_shift = cv2.GaussianBlur(dft_shift, (3, 1), 7)
 
 
-rows, cols = gray.shape
-crow,ccol = int(rows/2) , int(cols/2)
-# create a mask first, center square is 1, remaining all zeros
-mask = np.zeros((rows,cols,2),np.uint8)
-
-r_out = 150
-r_in = 25
-
-x, y = np.ogrid[:rows, :cols]
-mask_area = np.logical_and(((x - crow) ** 2 + (y - ccol) ** 2 >= r_in ** 2), ((x - crow) ** 2 + (y - ccol) ** 2 <= r_out ** 2))
-mask[mask_area] = 1
-#
+# magnitude_spectrum = 20*np.log(cv.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
 
 
-# apply mask and inverse DFT
-fshift = dft_shift*mask
+# rows, cols = gray.shape
+# crow,ccol = int(rows/2) , int(cols/2)
+# # create a mask first, center square is 1, remaining all zeros
+# mask = np.zeros((rows,cols,2),np.uint8)
 
-fshift_mask_mag = 20*np.log(cv.magnitude(fshift[:,:,0],fshift[:,:,1]))
+# r_out = 150
+# r_in = 25
+
+# x, y = np.ogrid[:rows, :cols]
+# mask_area = np.logical_and(((x - crow) ** 2 + (y - ccol) ** 2 >= r_in ** 2), ((x - crow) ** 2 + (y - ccol) ** 2 <= r_out ** 2))
+# mask[mask_area] = 1
+# #
 
 
-f_ishift = np.fft.ifftshift(fshift)
-img_back = cv.idft(f_ishift)
-img_back = cv.magnitude(img_back[:,:,0],img_back[:,:,1])
+# # apply mask and inverse DFT
+# fshift = dft_shift*mask
 
-plt.subplot(221),plt.imshow(gray, cmap = 'gray')
+# fshift_mask_mag = 20*np.log(cv.magnitude(fshift[:,:,0],fshift[:,:,1]))
+
+
+# f_ishift = np.fft.ifftshift(fshift)
+# img_back = cv.idft(f_ishift)
+# img_back = cv.magnitude(img_back[:,:,0],img_back[:,:,1])
+
+plt.subplot(221),plt.imshow(x, cmap = 'gray')
 plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(222),plt.imshow(magnitude_spectrum, cmap = 'gray')
-plt.title('Initial Magnitude'), plt.xticks([]), plt.yticks([])
-plt.subplot(223),plt.imshow(img_back, cmap = 'gray')
-plt.title('Output Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(224),plt.imshow(fshift_mask_mag, cmap = 'gray')
-plt.title('Final Magnitude'), plt.xticks([]), plt.yticks([])
 plt.show()
 
